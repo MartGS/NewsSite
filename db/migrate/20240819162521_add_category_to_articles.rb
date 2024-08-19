9c70fdedb3e42548bfa2a4879b8f06ca8bab7efc
Григорий Мартынов <grigorii@MacBook-Pro-GS.local>
2024-08-19 23:56:29 +0300

    Updated migration and added rake task to migrate data from field category_id to category.

--- db/migrate/20240819162521_add_category_to_articles.rb
+++ db/migrate/20240819162521_add_category_to_articles.rb
@@ -1,5 +1,5 @@
 class AddCategoryToArticles < ActiveRecord::Migration[7.1]
   def change
-    add_column :articles, :category, :string, null: false
+    add_column :articles, :category, :string
   end
 end
